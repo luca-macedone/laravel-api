c379db6aac48139360568f562be91af7412b7780
luca-macedone <luca.macedone@outlook.it>
2023-06-07 16:59:36 +0200

    minor changes

--- database/migrations/2023_06_07_133542_add_type_id_foreign_key_to_project_table.php
+++ database/migrations/2023_06_07_133542_add_type_id_foreign_key_to_project_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('project', function (Blueprint $table) {
+        Schema::table('projects', function (Blueprint $table) {
             $table->foreignId('type_id')->constrained();
         });
     }
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('project', function (Blueprint $table) {
+        Schema::table('projects', function (Blueprint $table) {
             $table->dropForeign('projects_type_id_foreign');
             $table->dropColumn('type_id');
         });
