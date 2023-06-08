db88b04a23455d8f26e14e2c9df5de28385c55af
luca-macedone <luca.macedone@outlook.it>
2023-06-08 17:33:23 +0200

    add tecnology seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,7 +22,8 @@ class DatabaseSeeder extends Seeder
         // ]);
         $this->call([
             ProjectSeeder::class,
-            TypeSeeder::Class
+            TypeSeeder::Class,
+            TecnologySeeder::Class
         ]);
     }
 }
