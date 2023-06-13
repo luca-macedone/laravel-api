d5581ee374aec0faeee782c12d06e833cbfcf0da
luca-macedone <luca.macedone@outlook.it>
2023-06-13 14:56:46 +0200

    add api/project controller

---
+++ app/Http/Controllers/API/ProjectController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectController extends Controller
+{
+    public function index(){
+
+        $projects = Project::with(['tecnologies', 'type', 'user'])->orderByDesc('id')->paginate(8);
+        
+        if(count($projects) > 0){
+            // if the db have something returns it
+            return response()->json([
+                'status' => true,
+                'projects' => $projects,
+            ]);
+
+        }else{
+            // if the db is empty return false
+            return response()->json([
+                'status' => false,
+                'projects' => null,
+            ]);
+
+        }
+    }
+}
