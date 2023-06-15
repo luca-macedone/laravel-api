ef04045ef6fa31d7736627be9b68e1bb1115edc3
luca-macedone <luca.macedone@outlook.it>
2023-06-15 17:26:37 +0200

    add single project api

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -4,26 +4,44 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Project;
-use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        // Official pagination: 9
+        $projects = Project::with(['technologies', 'type', 'user'])->orderByDesc('id')->paginate(3);
 
-        $projects = Project::with(['technologies', 'type', 'user'])->orderByDesc('id')->paginate(8);
-        
-        if(count($projects) > 0){
+        if (count($projects) > 0) {
             // if the db have something returns it
             return response()->json([
                 'status' => true,
                 'projects' => $projects,
             ]);
 
-        }else{
+        } else {
             // if the db is empty return false
             return response()->json([
                 'status' => false,
                 'projects' => null,
+            ]);
+
+        }
+    }
+
+    public function show($slug)
+    {
+        $result = Project::with(['technologies', 'type', 'user'])->where('slug', $slug)->first();
+
+        if ($result) {
+            return response()->json([
+                'status' => true,
+                'project' => $result,
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'project' => null,
             ]);
 
         }
